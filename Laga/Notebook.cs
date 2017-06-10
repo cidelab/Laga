8242530b5d66238a224eb40eeec0b3fb7b95b9ac
cidelab <cidelab@gmail.com>
2017-06-10 13:50:54 +0100

    Updating Code

---
+++ Laga/Notebook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Laga.Graphics
+{
+    public class Notebook
+    {
+            public Notebook()
+        {
+
+        }
+        /// <summary>
+        /// Print line by line in a textbox. For appareance and other properties refer to xaml. 
+        /// </summary>
+        /// <param name="textBox">the textbox</param>
+        /// <param name="arrMessages">array of strings</param>
+        /// <param name="clear">in case you want to clean the textbox before</param>
+        public static void PrintLines(TextBox textBox, string[] arrMessages, bool clear)
+        {
+            if (clear)
+            {
+                textBox.Clear();
+            }
+            else
+            {
+                textBox.AppendText("\r\n");
+            }
+
+            foreach (string r in arrMessages)
+            {
+                textBox.AppendText(r + "\r\n");
+            }
+
+        }
+
+    }
+}
