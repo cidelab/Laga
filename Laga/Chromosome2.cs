36e897b8d751ebab96c066fd6c3426d1774d278d
cidelab <cidelab@gmail.com>
2019-09-28 22:26:26 +0100

    working in the chromosome2

---
+++ Laga/Chromosome2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laga.GeneticAlgorithm
+{
+    /// <summary>
+    /// IDNA
+    /// </summary>
+    public class Chromosome2<T>
+    {
+
+        public int Count
+        {
+            get
+            {
+                return chromosome2.Count;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<T> chromosome2 { get; set; }
+
+        /// <summary>
+        /// cons 1
+        /// </summary>
+        public Chromosome2(int size)
+        {
+            chromosome2 = new List<T>(size);
+        }
+
+        public Chromosome2(List<T> lsTDNA)
+        {
+            chromosome2 = lsTDNA;
+        }
+
+        /// <summary>
+        /// Get Dna Chromosome at specific index
+        /// </summary>
+        /// <param name="index">index</param>
+        /// <returns>T</returns>
+        public T GetDNA(int index)
+        {
+            return chromosome2[index];
+        }
+
+        /// <summary>
+        /// Insert DNA in a chromosome at specific Location
+        /// </summary>
+        /// <param name="index">The location in the chromosome</param>
+        /// <param name="DNA">The DNA to insert</para
+        public void InsertDNA(int index, T DNA)
+        {
+            chromosome2[index] = DNA;
+        }
+
+        /// <summary>
+        /// Add DNA to the Chromosome
+        /// </summary>
+        /// <param name="DNA">The DNA type</param>
+        public void Add(T DNA)
+        {
+            chromosome2.Add(DNA);
+        }
+
+        /// <summary>
+        /// Converts DNA Chromosome to a String
+        /// </summary>
+        /// <param name="sep">separation</param>
+        /// <returns>string</returns>
+        public string Chrom2String(string sep)
+        {
+            return string.Join(sep, chromosome2);
+        }
+
+    }
+}
---
+++ Laga/Population2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laga.GeneticAlgorithm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Population2<T> : IEnumerable
+    {
+
+        private List<Chromosome2<T>> pop2;
+
+
+        /// <summary>
+        /// IEnumerator
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            return pop2.GetEnumerator();
+        }
+
+    }
+}
