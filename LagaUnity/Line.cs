3095cfb73086be23c069768af335f50171e78588
cidelab <cidelab@gmail.com>
2020-03-07 20:08:53 +0000

    testing 2

---
+++ LagaUnity/Line.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LagaUnity
+{
+    public class Line
+    {
+        private Point pa;
+        private Point pb;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Point PointA
+        {
+            get
+            {
+                return pa;
+            }
+            set
+            {
+                pa = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Point PointB
+        {
+            get
+            {
+                return pb;
+            }
+            set
+            {
+                pb = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pointA"></param>
+        /// <param name="pointB"></param>
+        public Line(Point pointA, Point pointB)
+        {
+            pa = pointA;
+            pb = pointB;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Point MidPoint()
+        {
+            return new Point((pa.X + pb.X) / 2, (pa.Y + pb.Y) / 2, (pa.Z + pa.Z) / 2);
+        }
+
+        /// <summary>
+        ///  Line point coordinates
+        /// </summary>
+        /// <returns>string</returns>
+        public override string ToString()
+        {
+            return "Line_" + PointA.ToString() + " - " + PointB.ToString(); 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="color"></param>
+        public void Draw(float width, Color color)
+        {
+            DrawLine(pa, pb, width, color);
+        }
+
+        /// <summary>
+        /// Draw a line
+        /// </summary>
+        /// <param name="pointA">The first point</param>
+        /// <param name="pointB"></param>
+        /// <param name="width"></param>
+        /// <param name="color"></param>
+        static public void DrawLine(Point pointA, Point pointB, float width, Color color)
+        {
+            GameObject line = new GameObject("Line " + pointA.ToString() + " - " + pointB.ToString());
+            LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+            lineRenderer.material.color = color;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, new Vector3(pointA.X, pointA.Y, pointA.Z));
+            lineRenderer.SetPosition(1, new Vector3(pointB.X, pointB.Y, pointB.Z));
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+        }
+    }
+}
---
+++ LagaUnity/Point.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using UnityEngine;
+
+namespace LagaUnity
+{
+    public class Point
+    {
+
+        private float x;
+        private float y;
+        private float z;
+
+        /// <summary>
+        /// The X coordinate
+        /// </summary>
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        /// <summary>
+        /// The Y coordinate
+        /// </summary>
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        /// <summary>
+        /// The Z coordinate
+        /// </summary>
+        public float Z
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;
+            }
+        }
+
+        /// <summary>
+        /// Build a laga point object by 3 coordinates.
+        /// </summary>
+        /// <param name="X">X coordinate</param>
+        /// <param name="Y">Y coordinate</param>
+        /// <param name="Z">z coordinate</param>
+        public Point(float X, float Y, float Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+
+        /// <summary>
+        /// Override string method, Print point coordinates
+        /// </summary>
+        /// <returns>string</returns>
+        public override string ToString()
+        {
+            return "(" + x + "," + y + "," + z + ")";
+        }
+
+        /// <summary>
+        /// Draw the point
+        /// </summary>
+        /// <param name="width">width for the point, 2.0</param>
+        /// <param name="color">the color's point to draw</param>
+        public void Draw(float width, Color color)
+        {
+            DrawPoint(this, width, color);
+        }
+
+        /// <summary>
+        /// Calculate the distance to the second point.
+        /// </summary>
+        /// <param name="pointB"></param>
+        /// <returns></returns>
+        public float DistanceTo(Point pointB)
+        {
+            return (float)Math.Sqrt(Math.Pow((X - pointB.X), 2) + Math.Pow((Y - pointB.Y), 2) + Math.Pow((Z - pointB.Z), 2));
+
+        }
+
+        /// <summary>
+        /// Draw a point
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="width"></param>
+        /// <param name="color"></param>
+        static public void DrawPoint(Point point, float width, Color color)
+        {
+            GameObject line = new GameObject("Point " + point.ToString());
+            LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+            lineRenderer.material.color = color;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, new Vector3(point.x - width / 3.0f, point.y - width / 3.0f, point.z));
+            lineRenderer.SetPosition(1, new Vector3(point.x + width / 3.0f, point.y + width / 3.0f, point.z));
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+        }
+
+    }
+}
