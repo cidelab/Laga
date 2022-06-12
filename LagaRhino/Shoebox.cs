a281b21f03f44044df2828eed2a5a1d45d9333f9
cidelab <cidelab@gmail.com>
2022-06-12 23:04:17 +0100

    shoebox

--- LagaRhino/Shoebox.cs
+++ LagaRhino/Shoebox.cs
@@ -12,97 +12,67 @@ namespace LagaRhino
     /// </summary>
     public class Shoebox : Brep
     {
+        private Point3d pa, pb, pc, pd, pe, pf, pg, ph;
         private Plane pl = Plane.WorldXY;
-        Brep br;
         private double uw;
         private double ul;
-        private double he;
 
         /// <summary>
         /// Get the ShoeBox geometry.
         /// </summary>
-        public Brep ShoeBox
-            { get { return br; } }
-
-        private Point3d pa, pb, pc, pd, pe, pf, pg, ph;
+        public Brep ShoeBox { get; }
 
         /// <summary>
         /// Length Shoebox Property
         /// </summary>
         public double Length
-        {
-            get
-            {
-                return ul;
-            }
-            set
-            {
-                ul = value;
-                ul = ul * 2;
-            }
-        }
+            { get { return ul; } }
+
         /// <summary>
         /// Width Shoebox property
         /// </summary>
         public double Width
-        {
-            get
-            {
-                return uw * 2;
-            }
-            set
-            {
-                uw = value;
-                uw = uw * 2;
-            }
-        }
-
-        /// <summary>
-        /// Height Shoebox property
-        /// </summary>
-        public double Heigth
-        {
-            get { return he; }
-            set { he = value;}
-        }
+            { get { return uw; } }
 
         /// <summary>
         /// Shoebox by length, width and height
         /// </summary>
         /// <param name="point">Base point location</param>
+        /// <param name="rotate">Rotate parameter</param>
         /// <param name="length">Length</param>
         /// <param name="width">Width</param>
         /// <param name="height">Height</param>
-        public Shoebox(Point3d point, double length, double width, double height)
+        public Shoebox(Point3d point, double length, double width, double height, double rotate = 0.0)
         {
-            PtsTranslate(point, length, width);
+            PtsTranslate(point, length, width, rotate);
 
             pe = pa;
             pf = pb;
             pg = pc;
             ph = pd;
 
-            pe.Z = he;
-            pf.Z = he;
-            pg.Z = he;
-            ph.Z = he;
+            pe.Z = height;
+            pf.Z = height;
+            pg.Z = height;
+            ph.Z = height;
 
-            br = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
+            ShoeBox = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
         }
 
         /// <summary>
         /// Shoebox with the free Z top points
         /// </summary>
         /// <param name="point">Base point location</param>
+        /// <param name="rotate">Rotate parameter</param>
         /// <param name="length">Length</param>
         /// <param name="width">Width</param>
         /// <param name="eHeight">Z parameter for e point</param>
         /// <param name="fHeight">Z parameter for f point</param>
         /// <param name="gHeight">Z parameter for g point</param>
         /// <param name="hHeight">Z parameter for h point</param>
-        public Shoebox(Point3d point, double length, double width, double eHeight, double fHeight, double gHeight, double hHeight)
+        public Shoebox(Point3d point, double length, double width, double eHeight, double fHeight, double gHeight, double hHeight, double rotate = 0.0)
         {
-            PtsTranslate(point, length, width);
+            PtsTranslate(point, length, width, rotate);
 
             pe = pa;
             pf = pb;
@@ -114,12 +84,14 @@ namespace LagaRhino
             pg.Z = gHeight;
             ph.Z = hHeight;
 
-            br = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
+           ShoeBox = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
         }
 
-        private void PtsTranslate(Point3d p, double l, double w)
+        private void PtsTranslate(Point3d p, double l, double w, double rot)
         {
             pl.Origin = p;
+            pl.Rotate(rot, pl.ZAxis);
+
             uw = w * 0.5;
             ul = l * 0.5;
 
