974f77060a93135c327ca3829b5846e9f744d21d
cidelab <cidelab@gmail.com>
2022-11-22 22:49:54 +0000

    Update code

---
+++ LagaLibrary/LagaRhino/LineCurveData.cs
@@ -0,0 +1,30 @@
+﻿using Rhino.Geometry;
+
+namespace LagaRhino
+{
+    public class LineCurveData
+    {
+        /// <summary>
+        /// Transform a LineCurve to an Axis object.
+        /// </summary>
+        /// <param name="axis">LineCurve</param>
+        /// <returns>LineCurve</returns>
+        public static LineCurve Axis(LineCurve axis)
+        {
+            Point3d pa = axis.PointAtStart;
+            Point3d pb = axis.PointAtEnd;
+
+            pa.Z = 0;
+            pb.Z = 0;
+
+            if (pb.DistanceTo(Point3d.Origin) < pa.DistanceTo(Point3d.Origin))
+            {
+                Point3d temp = pa;
+                pa = pb;
+                pb = temp;
+            }
+
+            return new LineCurve(pb, pa);
+        }
+    }
+}
---
+++ LagaLibrary/LagaRhino/Shoebox.cs
@@ -0,0 +1,106 @@
+﻿using Rhino.Geometry;
+
+namespace LagaRhino
+{
+    public class Shoebox : Brep
+    {
+        private Point3d pa, pb, pc, pd, pe, pf, pg, ph;
+        private Plane pl = Plane.WorldXY;
+        private double uw;
+        private double ul;
+        private double vol;
+        private double area;
+
+        /// <summary>
+        /// Get the ShoeBox geometry.
+        /// </summary>
+        public Brep ShoeBox { get; }
+
+        /// <summary>
+        /// Length Shoebox Property
+        /// </summary>
+        public double Volume
+        { get { return vol; } }
+
+        /// <summary>
+        /// Width Shoebox property
+        /// </summary>
+        public double Area
+        { get { return area; } }
+
+        /// <summary>
+        /// Shoebox by length, width and height
+        /// </summary>
+        /// <param name="point">Base point location</param>
+        /// <param name="rotate">Rotate parameter</param>
+        /// <param name="length">Length</param>
+        /// <param name="width">Width</param>
+        /// <param name="height">Height</param>
+        public Shoebox(Point3d point, double length, double width, double height, double rotate = 0.0)
+        {
+            PtsTranslate(point, length, width, rotate);
+
+            pe = pa;
+            pf = pb;
+            pg = pc;
+            ph = pd;
+
+            pe.Z = height;
+            pf.Z = height;
+            pg.Z = height;
+            ph.Z = height;
+
+            ShoeBox = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
+            Data();
+        }
+
+        /// <summary>
+        /// Shoebox with the free Z top points
+        /// </summary>
+        /// <param name="point">Base point location</param>
+        /// <param name="length">Length</param>
+        /// <param name="width">Width</param>
+        /// <param name="eHeight">Z parameter for e point</param>
+        /// <param name="fHeight">Z parameter for f point</param>
+        /// <param name="gHeight">Z parameter for g point</param>
+        /// <param name="hHeight">Z parameter for h point</param>
+        /// <param name="rotate">Rotate parameter</param>
+        public Shoebox(Point3d point, double length, double width, double eHeight, double fHeight, double gHeight, double hHeight, double rotate = 0.0)
+        {
+            PtsTranslate(point, length, width, rotate);
+
+            pe = pa;
+            pf = pb;
+            pg = pc;
+            ph = pd;
+
+            pe.Z = eHeight;
+            pf.Z = fHeight;
+            pg.Z = gHeight;
+            ph.Z = hHeight;
+
+            ShoeBox = Brep.CreateFromBox(new Point3d[] { pa, pb, pc, pd, pe, pf, pg, ph });
+            Data();
+
+        }
+
+        private void Data()
+        {
+            vol = ShoeBox.GetVolume();
+            area = ShoeBox.GetArea();
+        }
+        private void PtsTranslate(Point3d p, double l, double w, double rot)
+        {
+            pl.Origin = p;
+            pl.Rotate(rot, pl.ZAxis);
+
+            uw = w * 0.5;
+            ul = l * 0.5;
+
+            pa = pl.PointAt(-ul, -uw);
+            pb = pl.PointAt(ul, -uw);
+            pc = pl.PointAt(ul, uw);
+            pd = pl.PointAt(-ul, uw);
+        }
+    }
+}
