3de73a97fbc68c1f3f66948b5a75c13a85cca453
cidelab <cidelab@gmail.com>
2024-01-15 23:26:32 +0000

    updating code

--- LagaLibrary/LagaRhino/Properties/AssemblyInfo.cs
+++ LagaLibrary/LagaRhino/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LagaRhino")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rhinocommon methods and helper library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("designemergente.com")]
 [assembly: AssemblyProduct("LagaRhino")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Free")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
