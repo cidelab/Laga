1401ef8c2b57da057e55277da54b3e798876d677
cidelab <cidelab@gmail.com>
2024-11-05 15:51:27 +0000

    commit stagged

--- LagaLibrary/LagaExamples/TestingRandom.cs
+++ LagaLibrary/LagaExamples/TestingRandom.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Laga.GeneticAlgorithm;
+using Laga.Numbers;
 
 namespace LagaExamples
 {
@@ -11,6 +13,9 @@ namespace LagaExamples
         public static void Run()
         {
             Console.WriteLine("Running Basic Example 1...");
+            double dblRand = Rand.NextDouble(0, 1);
+            Console.WriteLine("random value between 0 and 1 (ex) is {0}", dblRand);
+            Console.ReadLine();
         }
     }
 }
